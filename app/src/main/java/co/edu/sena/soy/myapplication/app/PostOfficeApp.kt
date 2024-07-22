a594f9a7ca1b981d4f8cce53ba761dd1a4e2a912
N1c0lasD4z4H <ndazah8@gmail.com>
2024-07-21 21:33:01 -0500

    feature:se crea  archivo PostOfficeApp en el paquete app

---
+++ app/src/main/java/co/edu/sena/soy/myapplication/app/PostOfficeApp.kt
@@ -0,0 +1,16 @@
+package co.edu.sena.soy.myapplication.app
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun PostOfficeApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize,
+        color = Color.White
+
+    ){
+
+    }
+}
