4dc49e76215b5f20e4d5f9dc160ed78b51fd5adf
N1c0lasD4z4H <ndazah8@gmail.com>
2024-07-21 21:34:08 -0500

    Se actualiza main

--- app/src/main/java/co/edu/sena/soy/myapplication/MainActivity.kt
+++ app/src/main/java/co/edu/sena/soy/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.soy.myapplication.app.PostOfficeApp
 import co.edu.sena.soy.myapplication.ui.theme.LoginTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoginTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PostOfficeApp()
             }
         }
-    }
-}
+    }//
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoginTheme {
-        Greeting("Android")
-    }
-}
